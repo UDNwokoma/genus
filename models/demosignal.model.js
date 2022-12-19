0da7899893ec6d13c5b430c48971456f70ab1ab0
unknown <pubmail700@gmail.com>
2022-12-19 06:42:40 -0800

    first commit

---
+++ models/demosignal.model.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+//const collection = mongodb.db("myDB").collection("myCollection");
+const Schema = mongoose.Schema;
+  
+const DemosignalSchema = new Schema({
+  signal: {type: String,required: false,unique: false,trim: false,minlength: 0},
+  Bitcoin: {type: String,required: false,unique: false,trim: false,minlength: 0},
+  Ethereum: {type: String,required: false,unique: false,trim: false,minlength: 0},
+  eur_jpy: {type: String,required: false,unique: false,trim: false,minlength: 0},
+  usd_chf: {type: String,required: false,unique: false,trim: false,minlength: 0},
+  aud_cad: {type: String,required: false,unique: false,trim: false,minlength: 0},
+  eur_usd: {type: String,required: false,unique: false,trim: false,minlength: 0},
+  chf_jpy: {type: String,required: false,unique: false,trim: false,minlength: 0},
+  WCO: {type: String,required: false,unique: false,trim: false,minlength: 0},
+  NG: {type: String,required: false,unique: false,trim: false,minlength: 0},
+  BO: {type: String,required: false,unique: false,trim: false,minlength: 0},
+  Platinum: {type: String,required: false,unique: false,trim: false,minlength: 0},
+  Silver: {type: String,required: false,unique: false,trim: false,minlength: 0},
+  Gold: {type: String,required: false,unique: false,trim: false,minlength: 0},
+}, {
+  timestamps: true,
+});
+
+const demosignal = mongoose.model("demosignal",DemosignalSchema)
+
+module.exports = demosignal;
+
+
